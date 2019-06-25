6e9cee9b86bd90851ba9bda680c74a2217ffd0ab
Jonathan Niles <jonathanwniles@gmail.com>
2019-06-25 12:12:01 +0100

    buffer tests

--- email.js
+++ email.js
@@ -2,6 +2,15 @@ const axios = require('axios');
 const debug = require('debug')('clownfish:email');
 const fileType = require('file-type');
 
+const { Duplex } = require('stream');
+
+function bufferToStream(buffer) {
+  const stream = new Duplex();
+  stream.push(buffer);
+  stream.push(null);
+  return stream;
+}
+
 const relay = axios.create({ auth: { username: 'api', password: process.env.MAILGUN_API_KEY } });
 
 async function downloadAttachment(attachment) {
@@ -13,7 +22,7 @@ async function downloadAttachment(attachment) {
   debug(`Detected filetype: ${type.mime}`);
 
   return {
-    data: res.data,
+    data: bufferToStream(res.data),
     filename: attachment.name,
     mimeType: type.mime,
   };
