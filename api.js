c6061cc3818717b26a39a6dd0e5781099eabdaf4
Jonathan Niles <jonathanwniles@gmail.com>
2019-08-28 15:43:34 +0100

    feat: status page & dynamic folders
    Adds a status page with system information, and an interaction log.
    Also adds dynamic folder routing so that multiple folders can by managed
    from the same sunfish server.
    
    Closes #2.
    Closes #3.

--- api.js
+++ api.js
@@ -1,19 +1,20 @@
+const debug = require('debug')('clownfish');
 const drive = require('./drive');
-
-const parentReportId = process.env.GDRIVE_REPORT_DIR_ID;
 
 /**
  * @function findFolderIdByName
  *
  * @param {String} name - the name of the folder to locate in Google Drive.
+ * @param {String}  parentFolderId - the ID of the parent folder on Google Drive
  *
+ * @returns folderOrNull
  */
-async function findFolderIdByName(name) {
+async function findFolderIdByName(name, parentFolderId) {
   const { data } = await drive.files.list({
     q: `name="${name}"`,
     spaces: 'drive',
     fields: 'files(id, name)',
-    parents: [parentReportId],
+    parents: [parentFolderId],
   });
 
   if (data.files && data.files.length) {
@@ -27,13 +28,14 @@ async function findFolderIdByName(name) {
  * @function createFolder
  *
  * @param {String} name - the name of the folder to create in Google Drive
+ * @param {String}  parentFolderId - the ID of the parent folder on Google Drive
  *
  * @description
  * Creates a new folder in Google Drive underneath the top level directory.
  */
-async function createFolder(name) {
+async function createFolder(name, parentFolderId) {
   const mimeType = 'application/vnd.google-apps.folder';
-  const resource = { name, mimeType, parents: [parentReportId] };
+  const resource = { name, mimeType, parents: [parentFolderId] };
   const { data } = await drive.files.create({
     resource,
     fields: 'id',
@@ -42,5 +44,27 @@ async function createFolder(name) {
   return data;
 }
 
+/**
+ * @function ensureFolder
+ *
+ * @description
+ * Gracefully chains the findFolderIdByName() and createFolder() functions so that
+ * folder are either created or retrieved.
+ *
+ */
+async function ensureFolder(name, parentFolderId) {
+  let folder = await findFolderIdByName(name, parentFolderId);
+  if (!folder) {
+    debug(`Did not locate folder on Google Drive with name "${name}".  Creating a new one.`);
+
+    folder = await createFolder(name, parentFolderId);
+    debug('Folder created!');
+  }
+
+  // id where to upload the file
+  return folder.id;
+}
+
 exports.findFolderIdByName = findFolderIdByName;
 exports.createFolder = createFolder;
+exports.ensureFolder = ensureFolder;
