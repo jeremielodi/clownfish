0d251b0a9522d6753cddc95fe085f47dbf9ed6ed
mbayopanda <mbayopanda@gmail.com>
2019-10-08 09:47:26 +0100

    replace sqlite3 by better-sqlite3

--- logger.js
+++ logger.js
@@ -1,20 +1,14 @@
-const debug = require('debug')('sqlite');
-const sqlite3 = require('sqlite3').verbose();
+const Database = require('better-sqlite3');
 const dayjs = require('dayjs');
 dayjs.extend(require('dayjs/plugin/relativeTime'));
 
 /**
- * open database mode :
- * sqlite3.OPEN_CREATE | sqlite3.OPEN_READONLY | sqlite3.OPEN_READWRITE
+ * @class Logger
+ * @description this class allows to read and write logs in the database
  */
-const OPEN_MODE = 'sqlite3.OPEN_READWRITE';
-
 class Logger {
   constructor(name) {
-    this.db = new sqlite3.Database(name || './database.db', OPEN_MODE, (err) => {
-      if (err) { throw err; }
-      debug('Connected to the disk file database.');
-    });
+    this.db = new Database(name || './database.db');
   }
 
   write(params) {
@@ -22,68 +16,60 @@ class Logger {
     const end = dayjs(params.end).format('YYYY-MM-DD HH:mm:ss');
     const elapsed = dayjs(end).from(start, true);
     const parameters = {
-      $googleDriveParentFolderId: params.googleDriveParentFolderId,
-      $normalizedStructure: params.normalizedStructure,
-      $normalizedReportName: params.normalizedReportName,
-      $attachments: params.attachments,
-      $start: start,
-      $sender: params.sender,
-      $end: end,
-      $elapsed: elapsed,
+      googleDriveParentFolderId: params.googleDriveParentFolderId,
+      normalizedStructure: params.normalizedStructure,
+      normalizedReportName: params.normalizedReportName,
+      attachments: params.attachments,
+      start,
+      sender: params.sender,
+      end,
+      elapsed,
     };
-    this.db.serialize(() => {
-      this.db.run(`
-        CREATE TABLE IF NOT EXISTS logger (
-          googleDriveParentFolderId INTEGER,
-          normalizedStructure TEXT,
-          normalizedReportName TEXT,
-          attachments TEXT,
-          start TEXT,
-          sender TEXT,
-          end TEXT,
-          elapsed TEXT
-        )`);
 
-      this.db.run(`
-        INSERT INTO logger (googleDriveParentFolderId, normalizedStructure, normalizedReportName, attachments, start, sender, end, elapsed)
-        VALUES ($googleDriveParentFolderId, $normalizedStructure, $normalizedReportName, $attachments, $start, $sender, $end, $elapsed)
-      `, parameters);
-    });
+    const createLoggerTableQuery = `
+      CREATE TABLE IF NOT EXISTS logger (
+        googleDriveParentFolderId INTEGER,
+        normalizedStructure TEXT,
+        normalizedReportName TEXT,
+        attachments TEXT,
+        start TEXT,
+        sender TEXT,
+        end TEXT,
+        elapsed TEXT
+      )`;
+
+    this.db
+      .prepare(createLoggerTableQuery)
+      .run();
+
+    const insertIntoLoggerQuery = `
+      INSERT INTO logger (googleDriveParentFolderId, normalizedStructure, normalizedReportName, attachments, start, sender, end, elapsed)
+      VALUES ($googleDriveParentFolderId, $normalizedStructure, $normalizedReportName, $attachments, $start, $sender, $end, $elapsed)
+    `;
+
+    this.db
+      .prepare(insertIntoLoggerQuery)
+      .run(parameters);
   }
 
   read(n) {
     return new Promise((resolve, reject) => {
-      const limit = n ? ` LIMIT ${n}` : '';
-      const query = `
-        SELECT
-          googleDriveParentFolderId, normalizedStructure, normalizedReportName, attachments, start, sender, end, elapsed
-        FROM logger ORDER BY start DESC ${limit}
-        `;
-
-      this.db.all(query, (err, rows) => {
-        if (err) { return reject(err); }
+      try {
+        const limit = n ? ` LIMIT ${n}` : '';
+        const query = `
+          SELECT
+            googleDriveParentFolderId, normalizedStructure, normalizedReportName, attachments, start, sender, end, elapsed
+          FROM logger ORDER BY start DESC ${limit}
+          `;
+        const rows = this.db.prepare(query).all();
         rows.forEach((row) => {
           // eslint-disable-next-line no-param-reassign
           row.attachments = row.attachments ? JSON.parse(row.attachments) : [];
         });
         return resolve(rows);
-      });
-    });
-  }
-
-  size() {
-    return new Promise((resolve, reject) => {
-      this.db.get('SELECT COUNT(*) size FROM logger', (err, row) => {
-        if (err) { return reject(err); }
-        return resolve(row);
-      });
-    });
-  }
-
-  close() {
-    this.db.close((err) => {
-      if (err) { throw err; }
-      debug('Close the database connection.');
+      } catch (error) {
+        return reject(error);
+      }
     });
   }
 }
